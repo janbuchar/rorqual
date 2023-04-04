4ef4bfe927977f4a15e91923a5197219548940b6
Jan Buchar <Teyras@gmail.com>
2023-04-04 23:12:36 +0200

    Basic playlist display

--- rorqual.py
+++ rorqual.py
@@ -1,20 +1,34 @@
 import asyncio
+import os
 
 import httpx
+import typer
+from textual.features import FeatureFlag
 
 from rorqual.app import RorqualApp
 from rorqual.config import Config
 from rorqual.subsonic_client import SubsonicClient
+from rorqual.subsonic_player import SubsonicPlayer
 
 
-async def main():
-    config = Config.from_file()
+def main(dev: bool = False):
+    features = set[FeatureFlag]()
+    if dev:
+        features = features.union({"debug", "devtools"})
 
-    async with httpx.AsyncClient(base_url=config.subsonic_url) as httpx_client:
-        subsonic = SubsonicClient(httpx_client, config)
-        app = RorqualApp(subsonic)
-        await app.run_async()
+    os.environ["TEXTUAL"] = ",".join(sorted(features))
+
+    async def run_rorqual():
+        config = Config.from_file()
+
+        async with httpx.AsyncClient(base_url=config.subsonic_url) as httpx_client:
+            subsonic = SubsonicClient(httpx_client, config)
+            player = SubsonicPlayer(subsonic, asyncio.get_running_loop())
+            app = RorqualApp(subsonic, player)
+            await app.run_async()
+
+    asyncio.run(run_rorqual())
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    typer.run(main)
--- rorqual/app.py
+++ rorqual/app.py
@@ -1,22 +1,30 @@
 from itertools import groupby
 
 from textual.app import App, ComposeResult
-from textual.containers import Container
-from textual.widgets import Header, Tree
+from textual.containers import Container, Vertical
+from textual.message import Message
+from textual.reactive import reactive
+from textual.widget import Widget
+from textual.widgets import DataTable, Tree
 
 from rorqual.subsonic_client import SubsonicClient
-from subsonic.subsonic_rest_api import AlbumId3, ArtistId3
+from rorqual.subsonic_player import SubsonicPlayer
+from subsonic.subsonic_rest_api import AlbumId3, AlbumWithSongsId3, ArtistId3
 
 
-class RorqualApp(App):
-    BINDINGS = [("q", "quit", "Quit")]
+class AlbumTree(Widget):
+    class AddAlbumToPlaylist(Message):
+        def __init__(self, album: AlbumWithSongsId3) -> None:
+            super().__init__()
+            self.album = album
 
-    def __init__(self, subsonic: SubsonicClient) -> None:
-        super().__init__()
+    BINDINGS = [("j", "down", "Down"), ("k", "up", "Up"), ("a", "add_to_playlist", "Add to playlist")]
+
+    def __init__(self, subsonic: SubsonicClient, id: str) -> None:
+        super().__init__(id=id)
         self.subsonic = subsonic
 
     def compose(self) -> ComposeResult:
-        yield Header()
         yield Container(id="tree")
 
     async def on_mount(self):
@@ -27,6 +35,7 @@ class RorqualApp(App):
         }
 
         artists_tree = Tree[ArtistId3 | AlbumId3]("Artists")
+        artists_tree.guide_depth = 2
         artists_tree.root.expand()
 
         for artist in artists:
@@ -37,4 +46,65 @@ class RorqualApp(App):
             for album in albums.get(node.data.id, []):
                 node.add(f"[{album.year}] {album.name}", data=album)
 
-        self.query_one("#tree").mount(artists_tree)
+        self.query_one(Container).mount(artists_tree)
+
+    def action_down(self) -> None:
+        self.query_one(Tree).action_cursor_down()
+
+    def action_up(self) -> None:
+        self.query_one(Tree).action_cursor_up()
+
+    async def action_add_to_playlist(self) -> None:
+        node = self.query_one(Tree).cursor_node
+
+        if node and isinstance(node.data, AlbumId3):
+            album = await self.subsonic.get_album_details(node.data.id)
+            self.post_message(self.AddAlbumToPlaylist(album))
+
+
+class Playlist(Widget):
+    tracks = reactive(list[tuple[str, int | None, str]]())
+
+    def compose(self) -> ComposeResult:
+        table = DataTable()
+        table.add_columns("#", "Title")
+        yield table
+
+    def watch_tracks(self, new_tracks: list[tuple[str, int | None, str]]) -> None:
+        table = self.query_one(DataTable)
+        assert table is not None
+        table.clear()
+
+        for id, track_number, title in new_tracks:
+            table.add_row(track_number, title)
+
+
+class RorqualApp(App):
+    BINDINGS = [("q", "quit", "Quit")]
+
+    DEFAULT_CSS = """
+    #album-tree {
+        dock: left;
+        width: 25%;
+        height: 100%;
+    }
+
+    #playlist {
+        height: 100%;
+    }
+    """
+
+    def __init__(self, subsonic: SubsonicClient, player: SubsonicPlayer) -> None:
+        super().__init__()
+        self.subsonic = subsonic
+        self.player = player
+
+    def compose(self) -> ComposeResult:
+        yield AlbumTree(self.subsonic, id="album-tree")
+        with Vertical():
+            yield Playlist(id="playlist")
+
+    def on_album_tree_add_album_to_playlist(self, message: AlbumTree.AddAlbumToPlaylist) -> None:
+        playlist = self.query_one(Playlist)
+        assert playlist is not None
+        playlist.tracks = [(song.id, song.track, song.title) for song in message.album.song]
