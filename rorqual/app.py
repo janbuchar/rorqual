864990f455a24da5cd0ef8114ffa950d989d45b4
Jan Buchar <Teyras@gmail.com>
2023-04-07 22:18:34 +0200

    Implement play/pause

--- rorqual/app.py
+++ rorqual/app.py
@@ -1,5 +1,6 @@
 from itertools import groupby
 
+from rich.console import RenderableType
 from textual.app import App, ComposeResult
 from textual.containers import Container, Vertical
 from textual.message import Message
@@ -9,7 +10,7 @@ from textual.widgets import DataTable, Tree
 
 from rorqual.subsonic_client import SubsonicClient
 from rorqual.subsonic_player import SubsonicPlayer
-from subsonic.subsonic_rest_api import AlbumId3, AlbumWithSongsId3, ArtistId3
+from subsonic.subsonic_rest_api import AlbumId3, AlbumWithSongsId3, ArtistId3, Child
 
 
 class AlbumTree(Widget):
@@ -26,8 +27,8 @@ class AlbumTree(Widget):
         ("a", "add_to_playlist", "Add to playlist"),
     ]
 
-    def __init__(self, subsonic: SubsonicClient, id: str) -> None:
-        super().__init__(id=id)
+    def __init__(self, subsonic: SubsonicClient) -> None:
+        super().__init__()
         self.subsonic = subsonic
 
     def compose(self) -> ComposeResult:
@@ -53,6 +54,7 @@ class AlbumTree(Widget):
                 node.add(f"[{album.year}] {album.name}", data=album)
 
         self.query_one(Container).mount(artists_tree)
+        artists_tree.focus()
 
     def action_down(self) -> None:
         self.query_one(Tree).action_cursor_down()
@@ -78,35 +80,83 @@ class AlbumTree(Widget):
             self.post_message(self.AddAlbumToPlaylist(album))
 
 
+def duration(seconds: int) -> str:
+    minutes, seconds = divmod(seconds, 60)
+    return f"{minutes}:{seconds:02}"
+
+
 class Playlist(Widget):
-    tracks = reactive(list[tuple[str, int | None, str]]())
+    tracks = reactive(list[Child]())
+
+    BINDINGS = [
+        ("j", "down", "Down"),
+        ("k", "up", "Up"),
+    ]
+
+    class TrackSelected(Message):
+        def __init__(self, track: Child) -> None:
+            super().__init__()
+            self.track = track
 
     def compose(self) -> ComposeResult:
         table = DataTable()
-        table.add_columns("#", "Title")
+        table.cursor_type = "row"
+        table.add_column("#", width=3)
+        table.add_column("Title")
+        table.add_column("Time", width=5)
         yield table
 
-    def watch_tracks(self, new_tracks: list[tuple[str, int | None, str]]) -> None:
+    def watch_tracks(self, new_tracks: list[Child]) -> None:
         table = self.query_one(DataTable)
         assert table is not None
         table.clear()
 
-        for id, track_number, title in new_tracks:
-            table.add_row(track_number, title)
+        for track in new_tracks:
+            table.add_row(track.track, track.title, duration(track.duration or 0), key=track.id)
+
+    def on_data_table_row_selected(self, message: DataTable.RowSelected) -> None:
+        for track in self.tracks:
+            if track.id == message.row_key.value:
+                self.post_message(self.TrackSelected(track))
+                return
+
+    def action_down(self) -> None:
+        self.query_one(DataTable).action_cursor_down()
+
+    def action_up(self) -> None:
+        self.query_one(DataTable).action_cursor_up()
+
+
+class PlaybackProgress(Widget):
+    track = reactive[Child | None](None)
+    position = reactive[int](0)
+
+    def render(self) -> RenderableType:
+        if self.track is None:
+            return "Not playing"
+
+        return f"{duration(self.position)} / {duration(self.track.duration or 0)}"
 
 
 class RorqualApp(App):
     BINDINGS = [("q", "quit", "Quit")]
 
     DEFAULT_CSS = """
-    #album-tree {
+    AlbumTree {
         dock: left;
         width: 25%;
+        min-width: 40;
         height: 100%;
     }
 
-    #playlist {
+    Playlist {
         height: 100%;
+    }
+
+    PlaybackProgress {
+        dock: bottom;
+        text-align: right;
+        height: 1;
     }
     """
 
@@ -116,11 +166,27 @@ class RorqualApp(App):
         self.player = player
 
     def compose(self) -> ComposeResult:
-        yield AlbumTree(self.subsonic, id="album-tree")
+        yield AlbumTree(self.subsonic)
         with Vertical():
-            yield Playlist(id="playlist")
+            yield Playlist()
+            yield PlaybackProgress()
+
+    def on_mount(self) -> None:
+        self.player.register_time_position_callback(self.on_time_pos_updated)
 
     def on_album_tree_add_album_to_playlist(self, message: AlbumTree.AddAlbumToPlaylist) -> None:
         playlist = self.query_one(Playlist)
-        assert playlist is not None
-        playlist.tracks = [(song.id, song.track, song.title) for song in message.album.song]
+        playlist.tracks = message.album.song
+
+    def on_playlist_track_selected(self, message: Playlist.TrackSelected) -> None:
+        playback_progress = self.query_one(PlaybackProgress)
+        playback_progress.track = message.track
+
+        if message.track.id != self.player.playing_track:
+            self.player.play(message.track.id)
+        else:
+            self.player.toggle_paused()
+
+    def on_time_pos_updated(self, time_pos: float | None) -> None:
+        playback_progress = self.query_one(PlaybackProgress)
+        playback_progress.position = int(time_pos or 0.0)
--- rorqual/subsonic_player.py
+++ rorqual/subsonic_player.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
 import asyncio
+from typing import Callable
 
 import httpx
-from mpv import MPV
+from mpv import MPV, MpvEvent, MpvEventID
 
 from .subsonic_client import Buffer, SubsonicClient
+
+TimePositionCallback = Callable[[float | None], None]
 
 
 class SubsonicPlayer:
@@ -14,8 +17,20 @@ class SubsonicPlayer:
     def __init__(self, client: SubsonicClient, loop: asyncio.AbstractEventLoop) -> None:
         self.client = client
         self.loop = loop
-        self.mpv = MPV()
-        self.mpv.register_stream_protocol(self.PROTOCOL, self._open)
+
+        self._mpv = MPV()
+        self._mpv.register_stream_protocol(self.PROTOCOL, self._open)
+        self._mpv.register_event_callback(self.handle_event)
+        self._mpv.observe_property("time-pos", self.dummy_property_handler)
+        self._mpv.observe_property("pause", self.dummy_property_handler)
+        self._mpv.observe_property("filename", self.dummy_property_handler)
+        self._mpv.observe_property("playlist-current-pos", self.dummy_property_handler)
+
+        self.time_pos_callback: TimePositionCallback | None = None
+
+        self._paused = False
+        self._playing_track: str | None = None
+        self._playlist = list[str]()
 
     def _open(self, url: str) -> SubsonicStreamFrontend:
         parsed_url = httpx.URL(url)
@@ -26,6 +41,32 @@ class SubsonicPlayer:
         buffer = Buffer()
         task = self.loop.create_task(self.client.stream(parsed_url.host, buffer))
         return SubsonicStreamFrontend(buffer, task, self.loop)
+
+    def play(self, id: str) -> None:
+        self._mpv.play(f"{self.PROTOCOL}://{id}")
+
+    def toggle_paused(self) -> None:
+        self._mpv.pause = not self._paused
+
+    def register_time_position_callback(self, callback: TimePositionCallback) -> None:
+        self.time_pos_callback = callback
+
+    @property
+    def playing_track(self) -> str | None:
+        return self._playing_track
+
+    def handle_event(self, event: MpvEvent) -> None:
+        match event.event_id.value:
+            case MpvEventID.PROPERTY_CHANGE if event.data:
+                if event.data.name == "time-pos" and self.time_pos_callback:
+                    self.time_pos_callback(event.data.value)
+                if event.data.name == "pause":
+                    self._paused = event.data.value
+                if event.data.name == "filename":
+                    self._playing_track = event.data.value
+
+    def dummy_property_handler(self, *args) -> None:
+        pass
 
 
 class SubsonicStreamFrontend:
