f8c9a38ad41c863b57ca3fd8409f743d1bf80e77
Jan Buchar <Teyras@gmail.com>
2023-04-27 20:45:07 +0200

    Make the playlist scrollable

--- rorqual/ui/playlist.py
+++ rorqual/ui/playlist.py
@@ -1,3 +1,4 @@
+from functools import cached_property
 from itertools import accumulate, groupby, pairwise
 from typing import cast
 
@@ -5,10 +6,11 @@ from rich.color import Color, ColorType
 from rich.emoji import Emoji
 from rich.segment import Segment
 from rich.style import Style
+from textual.geometry import Region, Size
 from textual.message import Message
 from textual.reactive import reactive
+from textual.scroll_view import ScrollView
 from textual.strip import Strip
-from textual.widget import Widget
 
 from rorqual.media_library import MediaLibrary
 from rorqual.stream_manager import FetchingState, StreamManager
@@ -18,14 +20,27 @@ from subsonic.subsonic_rest_api import AlbumId3, Child
 from .util import duration
 
 
-class Playlist(Widget, can_focus=True):
+class PlaylistRows(list[tuple[AlbumId3, list[Child]]]):
+    @cached_property
+    def sublist_stops(self) -> list[int]:
+        return list(accumulate((len(tracks) + 1 for _, tracks in self), initial=0))
+
+    @property
+    def total_size(self) -> int:
+        return sum(len(tracks) + 1 for _, tracks in self)
+
+    def sublist_index(self, y: int) -> int | None:
+        return next((i for i, (_, next_stop) in enumerate(pairwise(self.sublist_stops)) if y < next_stop), None)
+
+
+class Playlist(ScrollView, can_focus=True):
     tracks = reactive(list[Child]())
     track_index = reactive[int | None](None)
     playback_state = reactive[PlaybackState]("stopped")
     media_library = reactive[MediaLibrary | None](None)
 
     _highlighted_row = reactive[int](0)
-    _playlist_rows = reactive[list[tuple[AlbumId3, list[Child]]]]([])
+    _playlist_rows = reactive[PlaylistRows](PlaylistRows())
 
     muted_text = Style(color=Color(name="muted", type=ColorType.STANDARD, number=8))
     active_row = Style(
@@ -37,8 +52,11 @@ class Playlist(Widget, can_focus=True):
 
     def __init__(self, stream_manager: StreamManager):
         super().__init__()
+
         self._stream_manager = stream_manager
         self._fetching_state = dict[str, FetchingState]()
+
+        self.virtual_size = Size(self.size.width, 0)
 
     BINDINGS = [
         ("j", "down", "Down"),
@@ -61,28 +79,35 @@ class Playlist(Widget, can_focus=True):
         pass
 
     def render_line(self, y: int) -> Strip:
-        sublist_stops = list(accumulate((len(tracks) + 1 for _, tracks in self._playlist_rows), initial=0))
+        scroll_x, scroll_y = self.scroll_offset
+        y += scroll_y
+
+        sublist_stops = self._playlist_rows.sublist_stops
 
         if not self._playlist_rows or y >= sublist_stops[-1]:
             return Strip.blank(1)
 
-        sublist_index = next(i for i, (_, next_stop) in enumerate(pairwise(sublist_stops)) if y < next_stop)
+        sublist_index = self._playlist_rows.sublist_index(y)
+        assert sublist_index is not None
 
         if y in sublist_stops:
             album, tracks = self._playlist_rows[sublist_index]
-            return self.album_strip(album, tracks)
+            return self.album_strip(album, tracks).crop(scroll_x, scroll_x + self.size.width)
 
         sublist_track_index = y - sublist_stops[sublist_index] - 1
         track_index = y - sublist_index - 1
 
-        return self.track_strip(self._playlist_rows[sublist_index][1][sublist_track_index], track_index)
+        return self.track_strip(self._playlist_rows[sublist_index][1][sublist_track_index], track_index).crop(
+            scroll_x, scroll_x + self.size.width
+        )
 
     def album_strip(self, album: AlbumId3, tracks: list[Child]) -> Strip:
         album_duration = sum(track.duration or 0 for track in tracks)
         style = Style(bgcolor=Color(name="background", type=ColorType.STANDARD, number=8), bold=True)
 
+        max_width = self.size.width if self._playlist_rows.total_size < self.size.height else self.size.width - 2
         segments = [f"[{album.year}]", f"{album.artist} - {album.name}", duration(album_duration)]
-        available_space = max(1, self.size.width - len(segments) - sum(len(segment) for segment in segments))
+        available_space = max(1, max_width - len(segments) - sum(len(segment) for segment in segments))
 
         return Strip(
             [
@@ -125,8 +150,9 @@ class Playlist(Widget, can_focus=True):
             else:
                 icon = ""
 
+        max_width = self.size.width if self._playlist_rows.total_size < self.size.height else self.size.width - 2
         segments = [str(icon), f"{track.track or '':3}", track.title, duration(track.duration or 0)]
-        available_space = max(1, self.size.width - len(segments) - sum(len(segment) for segment in segments))
+        available_space = max(1, max_width - len(segments) - sum(len(segment) for segment in segments))
 
         return Strip(
             [
@@ -149,14 +175,23 @@ class Playlist(Widget, can_focus=True):
         for track in new_tracks:
             self._stream_manager.fetch(track.id)
 
-    def compute__playlist_rows(self) -> list[tuple[AlbumId3, list[Child]]]:
-        result = list[tuple[AlbumId3, list[Child]]]()
+    def compute__playlist_rows(self) -> PlaylistRows:
+        result = PlaylistRows()
 
         if self.media_library:
             for album_id, tracks in groupby(self.tracks, lambda it: it.album_id):
                 result.append((self.media_library.albums[cast(str, album_id)], list(tracks)))
 
         return result
+
+    def watch__playlist_rows(self, new_rows: PlaylistRows) -> None:
+        self.virtual_size = Size(
+            self.size.width if new_rows.total_size < self.size.height else self.size.width - 2, new_rows.total_size
+        )
+
+    def watch__highlighted_row(self, new_row: int) -> None:
+        new_row += self._playlist_rows.sublist_index(new_row) or 0
+        self.scroll_to_region(Region(0, new_row, self.size.width, 3))
 
     def on_fetch_state_change(self, id: str, state: FetchingState) -> None:
         self._fetching_state[id] = state
